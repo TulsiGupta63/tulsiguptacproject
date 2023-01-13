1ac767e120541dd428aa752855e58ad09765f224
Tulsigu <122440990+Tulsigu@users.noreply.github.com>
2023-01-13 13:34:27 +0530

    Create Q1. C program to perform all arithmetic operations. Input:.c

---
+++ Q1. C program to perform all arithmetic operations. Input:.c
@@ -0,0 +1,12 @@
+
+#include<stdio.h>
+int main(){
+ int a , b;
+ printf("Enter two numbers");
+ scanf("%d %d",&a,&b);
+ printf("sum of a and b is: %d\n",a+b);
+ printf("subtract of a and b is: %d\n",a-b);
+ printf("Multiplication of a and b is: %d\n",a*b);
+ printf("Divide of a and b is: %d\n",a/b);
+ printf("modulus of a and b is: %d",a%b);
+ return 0; }
