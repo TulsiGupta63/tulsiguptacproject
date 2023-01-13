212fe2474390f4f19aba95064308ead29825dc47
Tulsigu <122440990+Tulsigu@users.noreply.github.com>
2023-01-13 13:36:48 +0530

    Create Q5. C program to find power and square root of any  number. INPUT.c

---
+++ Q5. C program to find power and square root of any  number. INPUT.c
@@ -0,0 +1,11 @@
+
+#include <stdio.h>
+int main()
+{
+ double num, root;
+ printf("Enter any number to find square root: ");
+ scanf("%lf", &num);
+ root = sqrt(num);
+ printf("Square root of %.2lf = %.2lf", num, root);
+ return 0;
+}
