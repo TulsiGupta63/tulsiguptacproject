31beb07d93d8096c6b13eda375fe1a7f2d6949af
Tulsigu <122440990+Tulsigu@users.noreply.github.com>
2023-01-13 13:37:39 +0530

    Create Q6. C program to calculate total, average and percentage  and grades of five subjects. Input.c

---
+++ Q6. C program to calculate total, average and percentage  and grades of five subjects. Input.c
@@ -0,0 +1,17 @@
+
+#include <stdio.h>
+int main(){
+ float eng, phy, chem, math, comp; 
+ float total, average, percentage;
+ printf("Enter marks of five subjects: :- ");
+ scanf("%f%f%f%f%f", &eng, &phy, &chem, &math, &comp);
+ total = eng + phy + chem + math + comp;
+ average = total / 5.0;
+ percentage = (total / 500.0) * 100;
+ printf("Total marks = %.2f\n", total);
+ printf("Average marks = %.2f\n", average);
+ printf("Percentage = %.2f\n", percentage);
+ if (percentage<=100)
+ printf("O grade");
+ else if(percentage>=80)
+ printf("A+ grade");}
